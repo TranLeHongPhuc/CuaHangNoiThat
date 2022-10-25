f880c0fb1ea9b242e5759e37e27f5e9e15bb7975
CrisKhang <trank6531@gmail.com>
2022-10-25 14:26:06 +0700

    updateEntity

--- src/main/java/com/noithat/entity/Product.java
+++ src/main/java/com/noithat/entity/Product.java
@@ -1,5 +1,50 @@
 package com.noithat.entity;
 
-public class Product {
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
+
+@SuppressWarnings("serial")
+@Data
+@Entity
+@Table(name = "Products")
+public class Product implements Serializable{
+
+	@Id
+	Integer id;
+	String name;
+	String image;
+	Double price;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name = "CreateDate")
+	Date createDate = new Date();
+	
+	Boolean available;
+	
+	@ManyToOne
+	@JoinColumn(name = "Categoryid")
+	Category category;
+	
+	@ManyToOne
+	@JoinColumn(name = "Subcategoryid")
+	Subcategory subcategory;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "product")
+	List<OrderDetail> orderDetails;
 }
--- src/main/java/com/noithat/entity/Subcategory.java
+++ src/main/java/com/noithat/entity/Subcategory.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -15,15 +17,18 @@ import lombok.Data;
 @SuppressWarnings("serial")
 @Data
 @Entity
-@Table(name = "Roles")
-public class Role implements Serializable{
-
+@Table(name = "Subcategories")
+public class Subcategory implements Serializable{
+	
 	@Id
 	String id;
 	String name;
 	
 	@JsonIgnore
-	@OneToMany(mappedBy = "role")
-	List<Authority> authorities;
+	@OneToMany(mappedBy = "subcategory")
+	List<Product> products;
 	
+	@ManyToOne
+	@JoinColumn(name = "Categoryid")
+	Category category;
 }
