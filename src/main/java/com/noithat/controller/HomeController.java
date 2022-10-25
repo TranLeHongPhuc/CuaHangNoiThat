657e39fc6709ab8cb9cc2afcb990072f4d17d246
CrisKhang <trank6531@gmail.com>
2022-10-25 14:42:56 +0700

    Update HomeController.java

--- src/main/java/com/noithat/controller/HomeController.java
+++ src/main/java/com/noithat/controller/HomeController.java
@@ -2,10 +2,12 @@ package com.noithat.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	@GetMapping
+	
+	@RequestMapping("/home")
 	public String home() {
 		return "layout/index";
 	}
