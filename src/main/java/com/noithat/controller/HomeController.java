f9ea49d1a749010a5c9be1fbe353627cf052796a
Thien An <skyteamrules@gmail.com>
2022-10-26 15:28:39 +0700

    update HomeController

--- src/main/java/com/noithat/controller/HomeController.java
+++ src/main/java/com/noithat/controller/HomeController.java
@@ -1,12 +1,24 @@
 package com.noithat.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.noithat.entity.Product;
+import com.noithat.service.ProductService;
 
 @Controller
 public class HomeController {
+	@Autowired
+	ProductService productService;
+	
 	@RequestMapping("/home")
-	public String home() {
+	public String home(Model model) {
+		List<Product> list = productService.findAll();
+		model.addAttribute("items", list);
 		return "home/view";
 	}
 	
