e9e0110f9b2874f0832838cf18a90127b085808c
Lê Bảo Đăng <85282838+LeBaoDang@users.noreply.github.com>
2022-10-27 11:48:16 +0700

    update

--- src/main/resources/static/assets/admin/admin-app.js
+++ src/main/resources/static/assets/admin/admin-app.js
@@ -14,7 +14,15 @@ app.config(function($routeProvider){
 		templateUrl: "/assets/admin/orders/index.html",
 		controller: "orders-ctrl"
 	})
-	.when("/authorize", {
+	.when("/categories", {
+		templateUrl: "/assets/admin/categories/index.html",
+		controller: "categories-ctrl"
+	})
+	.when("/subcategories", {
+		templateUrl: "/assets/admin/subcategories/index.html",
+		controller: "subcategories-ctrl"
+	})
+	.when("/authority", {
 		templateUrl: "/assets/admin/authority/index.html",
 		controller: "authority-ctrl"
 	})
